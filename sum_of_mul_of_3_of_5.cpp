1d338abcb0171165354c69d20134f4e424535414
steephengeorge <steephengeorge@gmail.com>
2015-04-07 07:30:43 -0400

    Update sum_of_mul_of_3_of_5.cpp

--- sum_of_mul_of_3_of_5.cpp
+++ sum_of_mul_of_3_of_5.cpp
@@ -2,11 +2,15 @@
 
 int main()
 {
-   int sum=0;
+    unsigned int sum=0;
 
-   for(int i=0;i<1000; ++i)
-    if((i%3==0) || (i%5==0) || ( (i%3==0) && (i%5==0)))
-      sum+=i;   
-   std::cout<<sum<<std::endl;   
-                      
+    for( int i = 0; i < 1000; ++i )
+    {
+        if( ( i % 3 == 0 ) || ( i % 5 == 0 )  ||
+           ( ( i % 3 == 0 ) && ( i % 5 == 0 ) ) )
+        {
+            sum += i;  
+        }
+    }
+    std::cout<< sum <<"\n";                         
 }
