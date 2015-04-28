a2988616138a50c8f825cbfa0dcfa1ac289c6f9a
steephengeorge <steephengeorge@gmail.com>
2015-04-28 19:54:57 -0400

    Update permute.cpp

--- permute.cpp
+++ permute.cpp
@@ -1,43 +1,46 @@
 #include <iostream>
 #include<fstream>
 #include<algorithm>
- 
-void readInputFile(std::string filename)
- 
+#include<string>
+
+void create_permute(  std::string& record )
 {
-        //std::cout<<"readInputFile: filename:"<< filename<<std::endl;
-        std::ifstream infile;
-        infile.open(filename);
-        std::string record;
-        while(true )
-        {
-            std::getline(infile,record);
-            // std::cout<<"readInputFile: record:"<< record<<std::endl;
-            if(infile.eof())
-                break;
-            std::sort(record.begin(), record.end());
-            bool flag=0;
-            do{
-                   if(flag)
-                        std::cout<<",";
-                   flag=1;
-                   std::cout <<record;
-            } while(std::next_permutation(record.begin(), record.end()));
-            std::cout<<"\n";
- 
-        }
+    if( !record.empty() ) 
+    {
+        std::sort( record.begin(), record.end() );
+        bool flag=0;
+        do{
+            if( flag )
+            {
+                std::cout<<",";
+            }
+            flag = 1;
+            std::cout << record;
+        } while( std::next_permutation( record.begin(), record.end() ) );
+        std::cout << "\n";
+    }
+}
+void readInputFile( std::string filename ) 
+{
+    
+    std::ifstream infile;
+    infile.open( filename );
+    std::string record;
+    while( std::getline( infile,record ) )
+    {         
+       create_permute( record );
+    }
 }
  
-int main(int argc, char* argv[])
+int main( int argc, char* argv[] )
 {
-    if(argc < 1)
+    if( argc < 2 )
     {
-        std::cout<<"usage is filesize filename"<<std::endl;
-        exit(0);
+        std::cout << "usage: filesize filename" << "\n";
+        exit( 0 );
     }
-    std::ios_base::sync_with_stdio(false);
-    readInputFile(argv[1]);
-    //readInputFile();
- 
+    std::ios_base::sync_with_stdio( false );
+    readInputFile( argv[ 1 ] );
+    
     return 0;
 }
