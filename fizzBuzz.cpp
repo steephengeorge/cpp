de904309a96aee9148ddc46c1509555b028d9c15
steephengeorge <steephengeorge@gmail.com>
2015-05-09 11:30:20 -0400

    Update fizzBuzz.cpp

--- fizzBuzz.cpp
+++ fizzBuzz.cpp
@@ -10,35 +10,46 @@ struct input_rec{
     int last;
 };
  
-void parseRecord(std::string &record, input_rec& rec)
+void parseRecord( std::string &record, input_rec& rec )
 {
-    int first=0;
-    int second=0;
-    int last=0;
-    bool good=0;
+    int first  = 0;
+    int second = 0;
+    int last   = 0;
+    bool good  = 0;
    
     size_t start = 0;
-                size_t end =  record.find_first_of(" ");
-                first=std::atoi(record.substr(start,end-start).c_str());
-    if((1<=first) && (first <=20))
-        good=1;
+    size_t end =  record.find_first_of(" ");
+    first = std::atoi(record.substr(start,end-start).c_str());
+    if( ( 1 <= first ) && ( first <= 20 ) )
+    {
+        good = 1;
+    }
     else
+    {
         return;
+    }
   
-                start=end+1;
-                end=record.find_first_of(" ",start);
-    second=std::atoi(record.substr(start,end-start).c_str());
-                if(good && ((1<=second) && (second <=20)))
+    start = end + 1;
+    end = record.find_first_of( " ", start );
+    second = std::atoi( record.substr( start, end-start ).c_str() );
+    if( good && ( ( 1 <= second ) && ( second <= 20 ) ) )
+    {
          good=1;
+    }
     else
+    {
         return;
-   
-                start=end+1;
-                last =std::atoi(record.substr(start,std::string::npos).c_str());
-    if(good && ((21 <=last)&& (second <=200)))
-         good=1;
+    }
+    start = end + 1;
+    last = std::atoi( record.substr( start, std::string::npos ).c_str() );
+    if( good && ( (21 <= last ) && ( second <= 200 ) ) )
+    {
+        good=1;
+    }
     else
-         return;
+    {
+        return;
+    }
     if(good)
     {
         rec.first=first;
@@ -46,112 +57,109 @@ void parseRecord(std::string &record, input_rec& rec)
         rec.last=last;
     }
 }
-void readInputFile(std::string filename,std::vector<input_rec> & rec )
+void readInputFile( std::string fileName, std::vector< input_rec > & rec )
 {
                
-        //std::cout<<"readInputFile: filename:"<< filename<<std::endl;
-        std::ifstream infile;
-        infile.open(filename);
-                std::string record;
-                input_rec temp;
-                while(true )
-                {
-                                std::getline(infile,record);
-               // std::cout<<"readInputFile: record:"<< record<<std::endl;
-                if(infile.eof())
-                      break;
-                                parseRecord(record,temp);
-                                rec.emplace_back(temp);
-                }
+    //std::cout<<"readInputFile: filename:"<< filename<<std::endl;
+    std::ifstream infile( fileName );
+       
+    std::string record;
+    input_rec temp;
+    while( std::getline( infile, record ) )
+    {
+                                
+        parseRecord( record, temp );
+        rec.emplace_back( temp );
+    }
 }
-void checkfuzzbuzz(input_rec& rec)
+void checkfuzzbuzz( input_rec& rec )
 { 
-    int x=1;
-    int flag1 =0;
-    int flag2 =0;
-    while(x < rec.last)
+    int x     = 1;
+    int flag1 = 0;
+    int flag2 = 0;
+    
+    while( x < rec.last )
     {
-        if((x%rec.first)==0)
+        if( ( x % rec.first ) == 0)
         {
-            flag1=1;
+            flag1 = 1;
         }
-        if ((x%rec.second)==0)
+        if ( ( x % rec.second ) == 0 )
         {
-            flag2=1;
+            flag2 = 1;
         }
        
-        if((flag1+flag2)==2)
+        if( ( flag1 + flag2 ) == 2 )
         {
-            std::cout<<"FB"<<" ";           
+            std::cout << "FB" << " ";           
         }
-        else if(flag1 ==1)
+        else if( flag1 == 1)
         {
-            std::cout<<"F"<<" ";
+            std::cout << "F" << " ";
         }   
-        else if(flag2==1)
+        else if( flag2 == 1 )
         {
-            std::cout<<"B"<<" ";
+            std::cout << "B" << " ";
         }
         else
         {
-            std::cout<<x<<" ";
+            std::cout << x << " ";
         }
-        flag1=0;
-        flag2=0;
+        flag1 = 0;
+        flag2 = 0;
         ++x;
     }
-    if(x ==rec.last)
+    if( x == rec.last )
     {
-        if((x%rec.first)==0)
+        if( ( x % rec.first ) == 0 )
         {
-            flag1=1;
+            flag1 = 1;
         }
-        if ((x%rec.second)==0)
+        if ( ( x % rec.second ) == 0 )
         {
-            flag2=1;
+            flag2 = 1;
         }
        
-        if((flag1+flag2)==2)
+        if( ( flag1 + flag2 ) == 2 )
         {
-            std::cout<<"FB";           
+            std::cout << "FB";           
         }
-        else if(flag1 ==1)
+        else if( flag1 == 1)
         {
-            std::cout<<"F";
+            std::cout << "F";
         }   
-        else if(flag2==1)
+        else if( flag2 == 1 )
         {
-            std::cout<<"B";
+            std::cout << "B";
         }
         else
         {
-            std::cout<<x<<" ";
+            std::cout << x << " ";
         }
-        flag1=0;
-        flag2=0;
+        flag1 = 0;
+        flag2 = 0;
         ++x;
     }
-    std::cout<<std::endl;
+    std::cout << "\n";
 }
-int main(int argc, char* argv[])
+int main( int argc, char* argv[] )
 {
     auto start = std::chrono::steady_clock::now();
-    if(argc < 2)
+    if( argc < 2 )
     {
-        std::cout<<"usage: fuuzbuzz fuuzbuzz.txt"<<std::endl;
-        exit(0);
+        std::cout << "usage: fuuzbuzz fuuzbuzz.txt" << "\n";
+        exit( 0 );
     }
  
-    std::ios_base::sync_with_stdio(false);
-    std::vector<input_rec> input;
-    readInputFile(argv[1], input);    
+    std::ios_base::sync_with_stdio( false );
+    std::vector< input_rec > input;
+    readInputFile( argv[ 1 ], input );    
     for( auto &x : input)
     {
        checkfuzzbuzz(x);
     }
-   auto end = std::chrono::steady_clock::now();
-   auto duration= std::chrono::duration_cast<std::chrono::microseconds>(end-start);
-   std::cout<< duration.count()<<std::endl;
-    return 0;
+   auto end      = std::chrono::steady_clock::now();
+   auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end-start);
+   std::cout<< duration.count() << "\n";
+   return 0;
 }
- 
