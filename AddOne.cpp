ec69458265a493b7ecdc3286e70a68ee588a9078
steephengeorge <steephengeorge@gmail.com>
2016-05-04 00:41:22 -0400

    Create AddOne.cpp
    Given a non-negative number represented as an array of digits,
    
    add 1 to the number ( increment the number represented by the digits ).
    
    The digits are stored such that the most significant digit is at the head of the list.
    
    Example:
    
    If the vector has [1, 2, 3]
    
    the returned vector should be [1, 2, 4]
    
    as 123 + 1 = 124.

---
+++ AddOne.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <set>
+#include <vector>
+#include<algorithm>
+#include<cmath>
+#include<string>
+#include<limits>
+
+using namespace std;    
+
+vector<int> plusOne(vector<int> &A)
+{
+       
+    std::reverse(std::begin(A), std::end(A) );
+    std::vector<int> result;
+    int carry=1;
+    
+    for(unsigned int i =0; i < A.size(); ++i)
+    {  
+        int val = A[i] + carry;
+        carry= (val) / 10;
+        if(  carry > 0)
+        {
+            result.push_back( (A[i] + carry) % 10);
+            carry= (A[i] + carry) / 10;
+            continue;
+        }
+        result.push_back(val);      
+    }
+    if( carry > 0)
+    {
+        result.push_back(carry);
+    }
+    
+    
+        
+    std::reverse(std::begin(result), std::end(result) );
+    auto it = std::find_if( std::begin(result), std::end(result),[](int i){return i >= 1;});
+    if( it != std::end(result))
+    {        
+        result.erase(std::begin(result), it);
+    }
+    return result;
+}
+
+int main()
+{      
+    std::vector<int> X={1, 1, 1, 3, 2, 1, 1, 2, 5, 9, 6, 5};
+    std::vector<int> A = plusOne(X);
+    for( auto& ele: A)
+    {
+        std::cout<< ele;
+    }
+    
+}
