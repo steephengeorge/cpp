d7e723f4d3f7f1b6e1b4d7c7a47a24c2c123cab2
steephengeorge <steephengeorge@gmail.com>
2017-04-06 16:29:01 -0400

    prime necklace ring

--- prime_necklace_count.cpp
+++ prime_necklace_count.cpp
@@ -1,10 +1,62 @@
-//https://www.codeeval.com/open_challenges/187/
-#include<iostream>
-#include<fstream>
-#include<vector>
-#include<algorithm>
-#include<cstdlib>
-#include<chrono>
+/*
+
+Alice has an even number of N beads, and each bead has a number from 1 to N painted on it. She would like to make a necklace out of all the beads, with a special requirement: any two beads next to each other on the necklace must sum to a prime number. Alice needs your help to calculate how many ways it is possible to do so.
+
+For example:
+
+N = 4
+
+There are two possible ways to build the necklace. Note that the last bead connects to the first bead.
+
+1 2 3 4
+1 4 3 2
+Note: The necklace should be unique.
+For example: 1 2 3 4 is the same as 2 3 4 1 and 3 4 1 2 and 4 1 2 3.
+
+
+
+So the answer is 2.
+
+INPUT SAMPLE:
+
+The inputs consists of one even integer on a line. Each integer N is 2 <= N <= 18.
+
+For example:
+
+
+
+1
+2
+3
+2
+4
+5
+OUTPUT SAMPLE:
+
+Print a line containing the number of ways to make a necklace according to the above rules. If not even number print 0.
+
+For example:
+
+
+
+1
+2
+3
+1
+2
+0
+CONSTRAINTS:
+
+2 <= N <= 18
+Number of test cases is 5.
+
+*/
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <cstdlib>
+#include <chrono>
 
 
 //if Yes return true;
@@ -111,7 +163,8 @@ std::vector<std::vector<int>> insertEvenNumber(std::vector<std::vector<int>>& ve
     return eVecTemp;
 }
 
-inline void insert_first( std::vector<std::vector<int>>& vec, const int num )
+inline void insert_first( std::vector<std::vector<int>>& vec,
+                          const int num )
 {
     std::vector<int> temp;
     temp.emplace_back( 1 );
@@ -183,7 +236,7 @@ int create_consecutive_primes( const int num )
       return count;
 }
 
-void readInputFile( std::string filename )
+void readInputFile(const std::string filename )
 {
     std::ifstream infile( filename );
     std::string record;
@@ -215,6 +268,6 @@ int main( int argc, char* argv[] )
 
     auto end = std::chrono::steady_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::microseconds> ( end - start );
-    std::cout << duration.count() << "\n";    
+    std::cout << duration.count() << "\n";
 }
 
