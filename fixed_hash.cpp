8448c9fcb8730570737e1f8b3d7c8ea43c59a004
steephengeorge <steephengeorge@gmail.com>
2017-04-06 00:09:13 -0400

    fixed hash to print given number longest lines

--- fixed_hash.cpp
+++ fixed_hash.cpp
@@ -1,49 +1,58 @@
 //C++14 program:
 //
 
+   //Write a program which reads a file and prints to stdout the specified number of the
+   //longest lines that are sorted based on their length in descending order.
 
-  /*  Write a program which reads a file and prints to stdout the specified number of the longest lines that are sorted based on their length in descending order. Input sample:
+  /*  Write a program which reads a file and prints to stdout the specified number of
+    the longest lines that are sorted based on their length in descending order.
+    Input sample:
 
-    Your program should accept a path to a file as its first argument. The file contains multiple lines. The first line indicates the number of lines you should output, the other lines are of different length and are presented randomly. You may assume that the input file is formatted correctly and the number in the first line is a valid positive integer.
+    Your program should accept a path to a file as its first argument. The file contains
+    multiple lines. The first line indicates the number of lines you should output,
+    the other lines are of different length and are presented randomly. You may assume
+    that the input file is formatted correctly and the number in the first line is a valid
+    positive integer.
 
     For Example:
 
-    2 
+    2
     Hello World
-    CodeEval 
+    CodeEval
     Quick Fox
     A
     San Francisco
 
     Output sample:
 
-    Print out the longest lines limited by specified number and sorted by their length in descending order.
+    Print out the longest lines limited by specified number and sorted by their
+    length in descending order.
 
     For example:
 
-    San Francisco 
+    San Francisco
     Hello World
 
 */
 
-#include<iostream>
-#include<map>
-#include<fstream>
-#include<string>
-#include<chrono>
+#include <iostream>
+#include <map>
+#include <fstream>
+#include <string>
+#include <chrono>
 
 class fixed_hash{
 public:
-        fixed_hash( std::size_t pSize )
+        fixed_hash(std::size_t pSize)
         {
             size = pSize;
         }
-        void push( std::string & record )
+        void push(std::string & record)
         {
 
-            if( hash_table.size() < size )
+            if(hash_table.size() < size)
             {
-                hash_table.emplace( std::make_pair( record.size(), record ) );
+                hash_table.emplace(std::make_pair( record.size(), record ) );
             }
             else if( record.size() > hash_table.begin()->second.size() )
             {
@@ -53,8 +62,8 @@ public:
         }
         void print()
         {
-            auto end = std::rend(hash_table);            
-            for( auto it = std::rbegin( hash_table ); it != end ; ++it )            
+            auto end = std::rend(hash_table);
+            for( auto it = std::rbegin( hash_table ); it != end ; ++it )
             {
                 std::cout << it->second << "\n";
             }
@@ -67,31 +76,31 @@ public:
 
 void readInputFile( std::string fileName )
 {
-    std::ifstream infile( fileName );
+    std::ifstream infile(fileName);
     std::string record;
     int output_nbr = 0;
     infile >> output_nbr;
-    fixed_hash output_string_list( output_nbr );
-    while( std::getline( infile, record ) )
+    fixed_hash output_string_list(output_nbr);
+    while( std::getline(infile, record) )
     {
-        output_string_list.push( record );
+        output_string_list.push(record);
     }
 
     output_string_list.print();
     infile.close();
 }
 
-int main( int argc, char* argv[] )
+int main(int argc, char* argv[])
 {
     auto start = std::chrono::steady_clock::now();
-    if( argc < 2 )
+    if(argc < 2)
     {
         std::cout << "Error: input file is missing " << "\n";
-        exit( 0 );
+        return -1;
     }
 
-    std::ios_base::sync_with_stdio( false );
-    readInputFile( argv[ 1 ]);
+    std::ios_base::sync_with_stdio(false);
+    readInputFile(argv[ 1 ]);
 
     auto end      = std::chrono::steady_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end-start);
