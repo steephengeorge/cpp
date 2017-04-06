a22e0e2073b6ed4f6164ac389420b77f7f317640
steephengeorge <steephengeorge@gmail.com>
2017-04-05 22:59:59 -0400

    //updated using algorithm and refractored the code

--- CheckArrays_Permute.cpp
+++ CheckArrays_Permute.cpp
@@ -2,36 +2,28 @@
 #include<set>
 #include<algorithm>
 
-       
 bool ArraysPermute(int  array1[],int size1, int  array2[], int size2)
-{       
-       if( size1  != size2)           
+{
+        if( size1  != size2)
            return false;
-       else
-       {        
-           std::set<int> first_set(array1, array1+size1);
-           std::set<int> second_set(array2, array2+size2);
-       
-           std::pair<std::set<int>::iterator,std::set<int>::iterator> myPair=std::mismatch(first_set.begin(),first_set.end(),second_set.begin());
-          
-           if( *myPair.first != *myPair.second)
-                
-                return false;
-            else
-                return true;
-       }
+
+        std::multiset<int> first_set(array1, array1+size1);
+        std::multiset<int> second_set(array2, array2+size2);
+
+        return std::equal(first_set.begin(),first_set.end(),
+                                second_set.begin())? true: false;
 }
 
 
 int main()
-{     
-    int array1[] ={1,2,3,5};
-    int array2[]={1,2,4,3};
-    
+{
+    int array1[] = {1,2,3,6};
+    int array2[] = {1,2,2,3};
+
     if(ArraysPermute(array1,sizeof(array1)/sizeof(int), array2, sizeof(array2)/sizeof(int)))
-        std::cout<< " Arrays are permutation of each other\n"; 
+        std::cout<< " Arrays are permutation of each other\n";
     else
         std::cout<< " Arrays are not permutation of each other\n";
-       
+
     return 0;
 }
