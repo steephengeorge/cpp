2794f6c2ccbae51e9df5ccb1c548363a3ac23af5
steephengeorge <steephengeorge@gmail.com>
2017-04-06 00:39:22 -0400

    maximum sub_array

--- max_subarray.cpp
+++ max_subarray.cpp
@@ -1,43 +1,39 @@
 #include <iostream>
-#include <set>
 #include <vector>
-#include<algorithm>
-#include<cmath>
-#include<string>
-#include<limits>
+#include <algorithm>
 
-using namespace std;    
 
-int maxSubArray(const vector<int> &A)
+int maxSubArray(const std::vector<int> &A)
 {
-    
-    
-    int sum=A[0];
-    int max= *(std::max_element( A.begin(), A.end()));
-    
+
+
+    int sum = A[0];
+    int max = *(std::max_element( A.begin(), A.end()));
+
+    //If all number are negative in the given vector
     if( max < 0)
       return max;
+
     max=A[0];
-     
-    
-    for(unsigned int i =1; i< A.size(); ++i)
+
+    for(size_t i =1; i< A.size(); ++i)
     {
         sum += A[i];
         if( sum < 0 )
-        {            
+        {
             sum = 0;
         }
         max= std::max(sum, max);
-        
+
     }
-    
+
     max= std::max(sum, max);
     return max;
 }
 
 int main()
-{      
+{
     std::vector<int> X={-2, 1, -3, 4, -1, 2, 1, -5, 4 };
-    
+
     std::cout<<maxSubArray(X);
 }
