78584ba86213e2be967d180c90493a4b5d860779
steephengeorge <steephengeorge@gmail.com>
2017-04-07 19:39:40 -0400

    Maxinun of four integers

---
+++ MaxOfFourIntegers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int max_of_four(int a, int b, int c, int d)
+{
+
+   return ( std::max( std::max(a,b), std::max(c,d)));
+}
+int main() {
+    int a, b, c, d;
+    std::cin >> a>> b>> c >> d;
+    int ans = max_of_four(a, b, c, d);
+    std::cout << ans << "\n";
+}
