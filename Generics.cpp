43b2599f424a3964ded538dbb1a2cd6feee3dbae
steephengeorge <steephengeorge@gmail.com>
2020-01-11 23:19:59 -0800

    Moved source files to src directory

--- Generics.cpp
+++
@@ -1,8 +0,0 @@
-// Add your code here
-template<typename T>
-void printArray(std::vector<T> vec)
-{
-    for( auto& x : vec){
-        std::cout << x<<"\n";
-    }
-}
--- Scope.cpp
+++
@@ -1,9 +0,0 @@
-	// Add your code here
-    Difference(std::vector<int>& a){
-       std::copy(a.begin(),a.end(),std::back_inserter(elements));   
-    }
-    void computeDifference(){
-        int min = *std::min_element(elements.begin(), elements.end());
-        int max = *std::max_element(elements.begin(), elements.end());
-        maximumDifference = max - min;
-    }
